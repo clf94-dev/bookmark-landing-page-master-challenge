bc22668eb9b2dfc6a6ebb4df369d1474642a21f6
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-07-14 12:52:00 +0200

    features jquery added

--- bookmark-landing-page-master/app.js
+++ bookmark-landing-page-master/app.js
@@ -24,74 +24,45 @@ const navSlide = () => {
 };
 
 const carouselSlide = () => {
-    var myCarousel = $('#myCarousel');
-    var myNav = myCarousel.prev();
-    const oneClick = document.querySelector('#one-click');
-    const search = document.querySelector('#search');
-    const share = document.querySelector('#share');
-    var element = document.getElementById("one-click");
-    var element1 = document.getElementById("search");
-    var element2 = document.getElementById("share");
-
-    oneClick.onclick = () => {
-
-
-        myNav.find('button.active').removeClass("active");
-        element.classList.add("active");
-
-    };
-    search.onclick = () => {
-
-
-        myNav.find('button.active').removeClass("active");
-        element1.classList.add("active");
-    };
-    share.onclick = () => {
-
-        const indexShare = 3;
-        var newLI = myNav.children().eq(indexShare);
-
-        if (!newLI.hasClass('active')) {
-            myNav.find('button.active').removeClass('active');
-            newLI.addClass('active');
-        }
-
-    };
-    /*
-          buttonLinks.addEventListener('click', () => {
-            if (document.getElementById('one-click')) {
-                myNav.find('button.active').removeClass('active');
-                document.getElementsByClassName('one-click').addClass('active');
-            };
-            if (document.getElementById('search')) {
-                myNav.find('button.active').removeClass('active');
-                document.getElementsByClassName('search').addClass('active');
-            };
-            if (document.getElementById('share')) {
-                myNav.find('button.active').removeClass('active');
-                document.getElementsByClassName('share').addClass('active');
-            };
+    $("#one-click").click(function() {
+        $("#one-click").css({
+            "border-bottom-color": " hsl(0, 94%, 66%)",
+            "border-bottom-width": "3px"
         });
 
-    */
-    /* buttonLinks.addEventListener('click', () => {
-         buttonLinks.forEach((button, index) => {
-             console.log(index);
-             var newIndex = $(index);
-             myCarousel.carousel(newIndex);
-         });
+        $("#search").css("border-bottom-color", "white");
 
-     })
+        $("#share").css("border-bottom-color", "white");
+        $(".one-click").addClass("active");
+        $(".search").removeClass("active");
+        $(".share").removeClass("active");
+    });
+    $("#search").click(function() {
+        $("#search").css({
+            "border-bottom-color": " hsl(0, 94%, 66%)",
+            "border-bottom-width": "3px"
+        });
 
+        $("#one-click").css("border-bottom-color", "white");
 
-     myCarousel.on('slid.bs.carousel', function() {
-         var newIndex = $(this).find('.carousel-inner>.active').index();
-         var newLI = myNav.children().eq(newIndex);
-         if (!newLI.hasClass('active')) {
-             myNav.find('button.active').removeClass('active');
-             newLI.addClass('active');
-         }
-     });*/
-};
+        $("#share").css("border-bottom-color", "white");
+        $(".one-click").removeClass("active");
+        $(".search").addClass("active");
+        $(".share").removeClass("active");
+    });
+    $("#share").click(function() {
+        $("#share").css({
+            "border-bottom-color": " hsl(0, 94%, 66%)",
+            "border-bottom-width": "3px"
+        });
+
+        $("#one-click").css("border-bottom-color", "white");
+
+        $("#search").css("border-bottom-color", "white");
+        $(".one-click").removeClass("active");
+        $(".search").removeClass("active");
+        $(".share").addClass("active");
+    });
+}
 navSlide();
 carouselSlide();
